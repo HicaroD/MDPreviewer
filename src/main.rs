ba7edb286cc2fb164f96d77aed1f76eba1df990b
HicaroD <hdanrlley1@gmail.com>
2022-12-05 20:22:10 -0300

    feature(get-markdown_file): get path of markdown file from command line

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::{
+    fs::File,
+    io::prelude::*,
+    env,
+};
 use comrak::{markdown_to_html, ComrakOptions};
 
-fn main() {
+fn get_html(markdown_file_content: String) -> String {
+    let converted_markdown = markdown_to_html(&markdown_file_content, &ComrakOptions::default());
+    return converted_markdown;
+}
+
+fn get_file_path_from_command_line() -> String { 
+    let mut args = env::args().skip(1);
+
+    let path = match args.next() {
+        Some(path) => path,
+        None => {
+            eprintln!("You should set a path for a Markdown file in the command line argument");
+            std::process::exit(1);
+        }
+    };
+    return path;
+}
+
+fn read_markdown_file(file_path: String) -> String {
+    // TODO: improve error handling (don't use expect)
+    let mut markdown_file = File::open(file_path).expect("Should open file");
     let mut markdown_file_content = String::new();
-    let mut markdown_file = File::open("./README.md").expect("Should open file");
     markdown_file.read_to_string(&mut markdown_file_content).expect("Should read to string");
 
-    let converted_markdown = markdown_to_html(&markdown_file_content, &ComrakOptions::default());
-    println!("{}", converted_markdown);
+    return markdown_file_content;
+}
+
+fn main() {
+    let file_path = get_file_path_from_command_line();
+    let markdown_file_content = read_markdown_file(file_path);
+    let html = get_html(markdown_file_content);
+    println!("{}", html);
 }
